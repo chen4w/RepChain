c0489ceb4e96d1177011165eb268d1cfd8be1ad7
brightestboy <yafeng@iscas.ac.cn>
2018-09-07 16:24:11 +0800

    修改Udp为UdpConnected，并添加预执行和广播，TODO：重新构造适合repchain的交易

--- src/main/scala/rep/ui/web/UdpServer.scala
+++ src/main/scala/rep/ui/web/UdpServer.scala
@@ -1,63 +1,96 @@
 package rep.ui.web
 
 import java.net.InetSocketAddress
+import java.nio.charset.StandardCharsets
 
 import akka.actor.{Actor, ActorRef}
+import akka.pattern.ask
 import akka.io.{IO, Udp, UdpConnected}
-import akka.util.ByteString
+import akka.util.{ByteString, Timeout}
 import rep.app.conf.SystemProfile
 import rep.crypto.{ECDSASign, Sha256}
+import rep.network.PeerHelper
+import rep.network.base.ModuleHelper
+import rep.protos.peer.Transaction
 import rep.protos.transaction.IotTransaction
-import rep.ui.web.UdpServer.addr
+import rep.sc.Sandbox.DoTransactionResult
+import rep.sc.TransProcessor
+import rep.sc.TransProcessor.PreTransaction
+import rep.utils.GlobalUtils.ActorType
+
+import scala.concurrent.duration._
+import scala.concurrent.Await
 
 object UdpServer {
 
-  val addr:InetSocketAddress = new InetSocketAddress("0.0.0.0", SystemProfile.getUdpServicePort)
+  val local:InetSocketAddress = new InetSocketAddress("0.0.0.0", SystemProfile.getUdpServicePort)
+  val remote:InetSocketAddress = new InetSocketAddress("115.29.240.46",6000)
 
 }
 
 //#connected
-class UdpServer extends Actor {
+class UdpServer extends Actor with ModuleHelper{
 
+  import rep.ui.web.UdpServer.{ local, remote }
   import context.system
+  val timeout = Timeout(1000.seconds)
+  val sandbox = context.actorOf(TransProcessor.props("sandbox_udp", "", self), "sandboxUdpPost")
 
   override def preStart(): Unit = {
-    IO(Udp) ! Udp.Bind(self, addr)
-    print(s"UDP Server port:" + addr.getPort)
-  } // 绑定端口，进行监听;UdpConnected 为与指定的ip:port进行通信
+    // 绑定端口，进行监听;UdpConnected 为与指定的ip:port进行通信，避免其他未知client消息
+    IO(UdpConnected) ! UdpConnected.Connect(self, remote, Option(local))
+    print(s"UDP Server port:" + local.getPort)
+
+  }
 
   override def receive = {
-    case Udp.Bound(local) =>
+    case UdpConnected.Connected =>
       //#listener
-      print("hello country")
+      print("hello world")
       // 发送数据到指定云平台，来注册通道
-      sender ! Udp.Send(ByteString("ep=LX1537GRA8EHQ66S&pw=123456"), new InetSocketAddress("115.29.240.46",6000))
-      context.become(ready(sender()))
-//    case Udp.Received(data, remote) =>
-//      print(remote)
-//      print("hello world")
-  }
+      sender ! UdpConnected.Send(ByteString("ep=LX1537GRA8EHQ66S&pw=123456"), remote)
 
-  def ready(socket: ActorRef): Receive = {
-    case Udp.Received(data, remote) =>
-      val processed = // parse data etc., e.g. using PipelineStage
-      //#listener
-       print(data.utf8String)
+    case UdpConnected.Received(data) =>
+//      print(data.utf8String)
+      val back = data.utf8String + "back"
       try {
+        // 构建交易
         val tran = IotTransaction.parseFrom(data.toArray)
+        print(tran)
+        // 此处可能不一定是信任列表证书，可能是注册好的证书
         val cert = ECDSASign.getCertByNodeAddr(tran.cert.toStringUtf8)
         val tOutSig = tran.withSignature(com.google.protobuf.ByteString.EMPTY);
-        val flag = ECDSASign.verify(tran.signature.toByteArray, Sha256.hash(tOutSig.toByteArray), cert.get.getPublicKey)
-        print(tran)
-        print(flag)
+        ECDSASign.verify(tran.signature.toByteArray, Sha256.hash(tOutSig.toByteArray), cert.get.getPublicKey) match {
+          case true =>
+            // TODO execTran(tran, sender)
+            sender ! UdpConnected.Send(ByteString("签名验证成功", StandardCharsets.UTF_8), remote)
+          case false => sender ! UdpConnected.Send(ByteString("签名验证错误", StandardCharsets.UTF_8), remote)
+        }
       } catch {
-        case e:Exception => print(e.getMessage)
+        case e:Exception =>
+          print(e.getMessage)
+          sender ! UdpConnected.Send(ByteString(e.getMessage), remote)
       }
-      val back = data.utf8String + "back"
-      //TODO 把接收到的数据转为交易
-      //#listener
-      socket ! Udp.Send(ByteString(back), remote) // example server echoes back
-//    case Udp.Unbind  => socket ! Udp.Unbind
-//    case Udp.Unbound => context.stop(self)
+//      sender ! UdpConnected.Send(ByteString(back), remote) // example server echoes back
   }
+
+/*
+  // 预执行交易，并广播
+  def execTran(iotTransaction: IotTransaction, socket: ActorRef): Unit = {
+    val tran = iotTransaction
+    // TODO 此处需要将获取到的交易重新打包为RepChain可接受的交易
+     // val txr = PeerHelper.transactionCreator()
+    val future = sandbox ? PreTransaction(tran)
+    val result = Await.result(future, timeout.duration).asInstanceOf[DoTransactionResult]
+    val rv = result
+    rv.err match {
+      case None =>
+        //预执行正常,提交并广播交易
+        getActorRef(ActorType.TRANSACTION_POOL) ! tran // 给交易池发送消息 ！=》告知（getActorRef）
+        socket ! UdpConnected.Send(ByteString("Success"), remote)
+      case Some(e) =>
+        //预执行异常,废弃交易，向api调用者发送异常
+        socket ! UdpConnected.Send(ByteString(e.cause.getMessage), remote)
+    }
+  }*/
 }
